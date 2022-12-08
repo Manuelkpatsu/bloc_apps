1076c1a8d0db19a0ec77a6521e358160db9f803b
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2022-12-08 10:31:02 +0000

    Implement presentation layer

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,32 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:weather_app/app.dart';
+import 'package:weather_app/weather_bloc_observer.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:weather_repository/weather_repository.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: const Center(child: Text('Home Page')),
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = WeatherBlocObserver();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
+  runApp(WeatherApp(weatherRepository: WeatherRepository()));
 }
